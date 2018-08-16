a6fa93790e66d95a1f2497f083137f2e776f783c
huangjinfu <jinfu.huang@shanbay.com>
2018-08-16 11:14:05 +0800

    [WS-][Words] new data store

--- app/src/main/java/cn/hjf/lj/wsyt/WSYTActivity.java
+++ app/src/main/java/cn/hjf/lj/wsyt/WSYTActivity.java
@@ -9,19 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import cn.hjf.lj.R;
+import cn.hjf.lj.wsyt.data.PronunciationDataStore;
 
 public class WSYTActivity extends AppCompatActivity {
 
 	private Pronunciation mCurrent;
 	private List<Pronunciation> mData = new ArrayList<>();
+
+	private PronunciationDataStore mPronunciationDataStore;
 
 	private Random mRandom = new Random();
 
@@ -36,10 +37,12 @@ public class WSYTActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wsyt);
 
+		mPronunciationDataStore = new PronunciationDataStore(this);
+
 		mTextView = findViewById(R.id.tv);
 		mWriteView = findViewById(R.id.write_view);
 
-		mData = parse(readData());
+		mData = mPronunciationDataStore.getAll();
 
 		render();
 	}
@@ -50,68 +53,6 @@ public class WSYTActivity extends AppCompatActivity {
 		mMediaPlayer.release();
 
 		super.onDestroy();
-	}
-
-	private List<Pronunciation> parse(String str) {
-		List<Pronunciation> pList = new ArrayList<>();
-		String[] pArray = str.split("\n");
-		for (String s : pArray) {
-			s = s.replace("{", "");
-			s = s.replace("}", "");
-			String[] sA = s.split(",");
-
-			Pronunciation p = new Pronunciation();
-			p.setPing(sA[0]);
-			p.setPian(sA[1]);
-			p.setRoma(sA[2]);
-			p.setAudio(sA[3]);
-
-			pList.add(p);
-		}
-		return pList;
-	}
-
-	private String readData() {
-		AssetManager assetManager = getAssets();
-		InputStream is = null;
-		try {
-			is = assetManager.open("wsyt/wsyt_data.txt");
-			byte[] data = readFromInputStream(is);
-			String str = new String(data);
-			return str;
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		return null;
-	}
-
-	public static byte[] readFromInputStream(InputStream inputStream) {
-		if (inputStream == null) {
-			return null;
-		}
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		byte[] buffer = new byte[1024];
-		try {
-			int readCount = 0;
-			do {
-				baos.write(buffer, 0, readCount);
-				readCount = inputStream.read(buffer);
-			} while (readCount != -1);
-
-			byte[] result = baos.toByteArray();
-			baos.close();
-			return result;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 	public void next(View view) {
